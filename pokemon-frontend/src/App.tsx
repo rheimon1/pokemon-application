e44004e293ac8182cae807d1a006df7cec9d68d7
Wallace Ramon Nogueira Soares <wallace.soares@themiind.com>
2024-04-11 01:43:36 -0300

    feat: use pokemon ability component

--- pokemon-frontend/src/App.tsx
+++ pokemon-frontend/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PokemonAbilities from "./components/PokemonAbilities";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <PokemonAbilities />
     </div>
   );
-}
+};
 
 export default App;
